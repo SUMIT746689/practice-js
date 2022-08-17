17972014affb09b087ba113fe31a66c904465ebd
mehedi hasan <mehedihasansumit@gmail.com>
2022-08-17 09:24:55 +0600

    updated-8-17-22

--- multipleFetch.js
+++ multipleFetch.js
@@ -13,23 +13,7 @@ try{
     }
     nam();
 
-    //more relevant
-    const findAnyName = async() => {
-        const urls = [
-            "https://jsonplaceholder.typicode.com/posts/1",
-            "https://jsonplaceholder.typicode.com/users/1saf",
-            "https://jsonplaceholder.typicode.com/comments?postId=1"
-        ];
-          try{
-            let res = await Promise.all(urls.map(e => fetch(e)))
-            let resJson = await Promise.all(res.map(e => e.json()))
-            // resJson = resJson.map(e => e.results[0].name.first)
-            console.log(resJson)
-          }catch(err) {
-            console.log(err.message)
-          }
-        }
-        findAnyName()
+    console.log(window.scroll);
 }
 catch(err)  {
 console.log(err)
